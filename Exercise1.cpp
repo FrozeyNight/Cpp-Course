7f18db6df1339da8d2c0a5c7c67147e32d116234
FrozeyNight <frozeynight@gmail.com>
2025-09-27 21:23:44 +0200

    Refreshing my knowladge of C++

---
+++ Exercise1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Enter a number: ";
+
+    int userInput = 0;
+    std::cin >> userInput;
+
+    std::cout << userInput << "\n";
+
+    return 0;
+}
